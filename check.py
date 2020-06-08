9c2e7ecb1ecd456fb1c12336021f982e62a7cc2d
MichaelYanNJU <171240518@smail.nju.edu.cn>
2020-06-08 15:51:28 +0800

    Improve error reporting. Remove outdated comments.

--- check.py
+++ check.py
@@ -29,20 +29,19 @@ for data_in, data_out, ret_val in json.load(open(f_json)):
             to_spim_w.write(str(i) + '\n')
 
     ret = system(f"{program} {flags} {spim_file} < {spim_in} 2>/dev/null | sed -e s/'\(Enter an integer:\)\+'/''/ > {spim_out}")
-    # Suppose irsim is compiled by run.sh
     if ret != 0:
         with open(spim_out, 'r') as from_spim_r:
             err(data_in,
                 "runtime error occured when running your IR code\n")
     with open(spim_out, 'r') as from_spim_r:
-        # Filter out the first line "load ./workdir/a.ir"
-        try:
-            for idx, (expect, user_out) in enumerate(zip_longest(data_out, only_nums(from_spim_r))):
-                if expect != user_out:
-                    err(data_in, "Output mismatch! expected %d, found %d at line %d" % (expect, user_out, idx));
-            else:
-                pass
-        except ValueError:
-            err(data_in, "Output mismatch!(you output less than supposed?)")
+        for idx, (expect, user_out) in enumerate(zip_longest(data_out, only_nums(from_spim_r))):
+            if expect == None:
+                err(data_in, "Output mismatch!(you output more than supposed)")
+            if user_out == None:
+                err(data_in, "Output mismatch!(you output less than supposed)")
+            if expect != user_out:
+                err(data_in, "Output mismatch! expected %d, found %d at line %d" % (expect, user_out, idx));
+        else:
+            pass
 
 exit(0)
